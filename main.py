16595db6949704a971b5b45eeea9f24b07edda0b
fangjiong1990 <454001372@qq.com>
2020-12-06 13:57:29 +0800

    2020/12/06第二次提交

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ def print_hi(name):
     print(datetime.datetime.now())
     print(strftime('%Y-%m-%d %H:%M:%S', localtime()))
 
+    for i in range(1,10):
+        for j in range(1, i+1):
+            print(j, "*", i, "=", i*j, end=" ")
+        print()
+
 
 if __name__ == '__main__':
     print_hi('PyCharm')
